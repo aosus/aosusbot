72e5260c832577793c4540cc09734daed5d67005
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-08-03 20:22:30 +0300

    add super user id

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 
-TOKEN = str() # Enter bot token here
+"""
+get bot token from https://t.me/BotFather
+
+get user id from https://t.me/userinfobot
+"""
+
+TOKEN = str("") # Enter bot token here
+
+super_users = [] # id for super users
